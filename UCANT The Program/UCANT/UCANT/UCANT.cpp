e397e69d3671d43f811d87cfc469973d95eb4a58
ah682 <96323387+ah682@users.noreply.github.com>
2023-03-07 03:45:58 +0000

    Update

--- UCANT The Program/UCANT/UCANT/UCANT.cpp
+++ UCANT The Program/UCANT/UCANT/UCANT.cpp
@@ -9,13 +9,17 @@
 
 using namespace std;
 
-vector<string> readPlayerFile(string filename, int startChar, int endChar);
+vector<string> readCardType(string filename, int startChar, int endChar);
+
+vector<string> readCardName(string filename);
 
 time_t readSeedFile(string filename);
 
 int Random(int max);
 
 string drawCard(string player, vector <string> file_contents);
+
+string drawName(string player, vector <string> file_contents);
 
 void startRound(int round);
 
@@ -30,6 +34,7 @@ class CCard {
     string power;
     string resilence;
     string type;
+    string name;
 };
 
 struct SProfessor {
@@ -46,11 +51,14 @@ int main()
     // Welcomes message
     cout << "Welcome to U-Can't. Let the winnowing begin...' " << endl << endl;
 
-    // Reads file which holds deck for player 1 named plagiarist
-    vector <string> player1_file_contents = readPlayerFile("plagiarist.txt", 1, 1);
+    // Reads file which holds deck for player 1 named plagiarist card types
+    vector <string> player1_file_contents_type = readCardType("plagiarist.txt", 1, 1);
 
-    // Read files which holds deck for player 2 named piffle-paper
-    vector <string> player2_file_contents = readPlayerFile("piffle-paper.txt", 1, 1);
+    // Reads file which holds deck for player 1 named plagiarist card names
+    vector <string> player1_file_contents_name = readCardName("plagiarist.txt");
+
+    // Read files which holds deck for player 2 named piffle-paper type
+    vector <string> player2_file_contents_type = readCardType("piffle-paper.txt", 1, 1);
 
     //drawCard("Player 1", player2_file_contents);
     const int deckSize = 49;
@@ -60,12 +68,13 @@ int main()
 
      // Draw cards for player 1
     for (int i = 0; i < deckSize; i++) {
-        cardsplayer1[i].type = drawCard("Player 1", player1_file_contents);
+        cardsplayer1[i].type = drawCard("Player 1", player1_file_contents_type);
+        cardsplayer1[i].name = drawName("Player 1", player1_file_contents_name);
     }
 
     // Draw cards for player 2
     for (int i = 0; i < deckSize; i++) {
-        cardsplayer2[i].type = drawCard("Player 2", player2_file_contents);
+        cardsplayer2[i].type = drawCard("Player 2", player2_file_contents_type);
     }
 
     // Deallocate Memory
@@ -79,7 +88,7 @@ int Random(int max)
     return int(float(rand()) / (RAND_MAX + 1) * float(max));
 }
 
-vector<string> readPlayerFile(string filename, int startChar, int endChar) {
+vector<string> readCardType(string filename, int startChar, int endChar) {
     vector<string> file_contents;
 
     // Read from the file
@@ -109,6 +118,34 @@ vector<string> readPlayerFile(string filename, int startChar, int endChar) {
             getline(file_stream, line);
             if (line.size() >= charCount) {
                 file_contents.push_back(line.substr(0, charCount));
+            }
+        }
+
+        file_stream.close();
+    }
+    else {
+        std::cout << "Unable to open file " << filename << '\n';
+    }
+
+    return file_contents;
+}
+
+vector<string> readCardName(string filename) {
+    vector<string> file_contents;
+
+    // Read from the file
+    ifstream file_stream(filename);
+    if (file_stream.is_open()) {
+        string line;
+        while (getline(file_stream, line)) {
+            // Find the positions of the first and second whitespace characters
+            size_t pos1 = line.find_first_of(" \t");
+            size_t pos2 = line.find_first_of(" \t", pos1 + 1);
+
+            // If two whitespace characters are found, extract the card name
+            if (pos2 != string::npos) {
+                string name = line.substr(pos2 + 1);
+                file_contents.push_back(name);
             }
         }
 
@@ -164,7 +201,25 @@ string drawCard(string player, vector <string> file_contents) {
     return "";
 }
 
+string drawName(string player, vector <string> file_contents) {
+    // Chooses random number
+    int randomCard = Random(file_contents.size());
+
+    // Check if the vector has enough elements
+    if (randomCard >= file_contents.size()) {
+        cout << "Error: " << player << " has no cards to draw." << endl;
+        return "";
+    }
+
+    // Chooses random card
+    string file_contents_random = file_contents[randomCard];
+
+    // Announces which player has drawn a card and which card
+    cout << player << " has drawn:" << endl << file_contents_random << endl;
+    return file_contents_random;
+}
+
 void gameOver()
 {
-    cout << "Game Over!" << endl;
+    cout << "Game Over!" << endl << " " << "wins";
 }
